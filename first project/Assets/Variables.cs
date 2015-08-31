7f196cb94fceb746053252806b7b6f2b29668b69
unknown <10530098@CS514-21.ad.uvu.edu>
2015-08-31 10:04:52 -0600

    variables and functions assignment

---
+++ first project/Assets/Variables.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variables : MonoBehaviour 
+	{
+		int a = 60;
+
+
+		void Start ()
+		{
+			a = MultiplyByTwo (a);
+			Debug.Log (a);
+		}
+
+		int MultiplyByTwo (int number)
+		{
+			int ret;
+			ret = number * 10;
+			return ret;
+		}
+	}
